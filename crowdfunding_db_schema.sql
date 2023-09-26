abfd182fedc1305bc037b8496df0d90ada6477ca
gamuhrai <108629991+gamuhrai@users.noreply.github.com>
2023-09-25 23:07:02 -0400

    latest changes

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -4,57 +4,57 @@
 
 
 CREATE TABLE "category" (
-    "category_id" varchar(4)   NOT NULL,
-    "category" varchar(30)   NOT NULL,
+    "category_id" varchar(400)   NOT NULL,
+    "category" varchar(300)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
      )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" varchar(7)   NOT NULL,
-    "category" varchar(30)   NOT NULL,
+    "subcategory_id" varchar(700)   NOT NULL,
+    "category" varchar(300)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(300)   NOT NULL,
+    "last_name" varchar(300)   NOT NULL,
+    "email" varchar(600)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
      )
 );
 
 CREATE TABLE "campaign" (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
-    "company_name" varchar(30)   NOT NULL,
-    "description" varchar(50)   NOT NULL,
+    "company_name" varchar(300)   NOT NULL,
+    "description" varchar(100)   NOT NULL,
     "goal" float   NOT NULL,
     "pledged" float   NOT NULL,
-    "outcome" varchar(10)   NOT NULL,
+    "outcome" varchar(100)   NOT NULL,
     "backers_count" int   NOT NULL,
-    "country" varchar(3)   NOT NULL,
-    "currency" varchar(4)   NOT NULL,
+    "country" varchar(100)   NOT NULL,
+    "currency" varchar(100)   NOT NULL,
     "launched_date" date   NOT NULL,
     "end_date" date   NOT NULL,
-    "category_id" varchar(4)   NOT NULL,
-    "subcategory_id" varchar(7)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
+    "category_id" varchar(100)   NOT NULL,
+    "subcategory_id" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY ("cf_id"),
+    
+     );
+	 
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
 
-CREATE TABLE "contacts" (
-    "contact_id" int   NOT NULL,
-    "first_name" varchar(30)   NOT NULL,
-    "last_name" varchar(30)   NOT NULL,
-    "email" varchar(60)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     )
-);
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
 
-ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
-REFERENCES "campaign" ("category_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
 
-ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "campaign" ("subcategory_id");
-
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "campaign" ("contact_id");
-
+select * from campaign
